08fe6b4448ccf1a91c2fb6f815a121e77492ee10
Maksym Bezhevets <begik4@gmail.com>
2024-05-30 15:13:03 +0300

    deleted all comments

--- custom_reg_form/admin.py
+++ custom_reg_form/admin.py
@@ -2,13 +2,3 @@ from django.contrib import admin
 from .models import ExtraInfo
 
 admin.site.register(ExtraInfo)
-
-# from django.contrib import admin
-# from .models import ExtraInfo
-#
-#
-# @admin.register(ExtraInfo)
-# class ExtraInfoAdmin(admin.ModelAdmin):
-#     list_display = ("user", "categories")
-#     search_fields = ("user",)
-#     fields = ("user",)
--- custom_reg_form/models.py
+++ custom_reg_form/models.py
@@ -28,29 +28,3 @@ class ExtraInfo(models.Model):
         max_length=100,
     )
     categories = models.ManyToManyField("Category", related_name="extra_info")
-
-
-# from django.conf import settings
-# from django.db import models
-#
-# # Backwards compatible settings.AUTH_USER_MODEL
-# USER_MODEL = getattr(settings, "AUTH_USER_MODEL", "auth.User")
-#
-#
-# class ExtraInfo(models.Model):
-#     """
-#     This model contains two extra fields that will be saved when a user registers.
-#     The form that wraps this model is in the forms.py file.
-#     """
-#
-#     user = models.OneToOneField(
-#         USER_MODEL, null=True, related_name="user+", on_delete=models.CASCADE
-#     )
-#     categories = models.CharField(
-#         verbose_name="Interests",
-#         max_length=500,
-#     )
-#
-#     def __str__(self):
-#         result = "{0.user} {0.interests}"
-#         return result.format(self)
